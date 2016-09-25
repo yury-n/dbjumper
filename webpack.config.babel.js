efeee570300e35f249952deaa71d08c018cf390f
yury-n <yury.nedomolkin@gmail.com>
2016-09-25 12:13:52 +0400

    Rename "src" folder to "client"

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,7 +2,7 @@ import path from 'path';
 
 export default {
     devtool: 'eval',
-    entry: './src/index',
+    entry: './client/index',
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
